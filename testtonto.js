47331329e4aa7f580e834dc367e59503eb1e3b02
Almudena Vivanco <46971000+MrsDaehin@users.noreply.github.com>
2023-10-19 10:05:32 +0200

    Create testtonto.js

---
+++ testtonto.js
@@ -0,0 +1,27 @@
+import { testtonto } from "./tests/testtonto.js";
+import {
+  describe,
+  expect,
+} from "https://jslib.k6.io/k6chaijs/4.3.4.2/index.js";
+
+
+export const options = {};
+
+globalThis.vars = [];
+
+globalThis.pauseMin = 5;
+globalThis.pauseMax = 15;
+
+export default function main() {
+ 
+  describe("testtonto", () => {
+   
+    let test = testtonto();
+    
+    console.log(test);
+    
+    //expect(test.status, "test status").to.equal(200);
+    expect(JSON.parse(test.body).status, 'status code').to.equal('200.');
+    expect(test).to.have.validJsonBody();
+});
+}
